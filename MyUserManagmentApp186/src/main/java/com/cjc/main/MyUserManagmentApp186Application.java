306ec5b112a787877144c6f2f1a014c349fb0134
prati <prati@Pratik>
2025-02-28 11:14:06 +0530

    inital commit

---
+++ MyUserManagmentApp186/src/main/java/com/cjc/main/MyUserManagmentApp186Application.java
@@ -0,0 +1,13 @@
+package com.cjc.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyUserManagmentApp186Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyUserManagmentApp186Application.class, args);
+	}
+
+}
